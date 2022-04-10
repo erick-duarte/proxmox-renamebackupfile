cab2a044125bbe94f71a6fb4ff9327b85bea4b35
Erick Duarte <59427098+erick-duarte@users.noreply.github.com>
2022-04-10 16:37:39 -0300

    fix
    fix

--- renameBackupFile.sh
+++ renameBackupFile.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 directory="/lvm-disk4tb"
-cmd=(`ls $directory/dump/ | grep .log`)
+files=(`ls -f $directory/dump/ | grep .log`)
 
-for i in $cmd; do
+for i in $(echo ${files[@]}); do
+     echo "For result: $i"
+     
      vmname=$(cat $directory/dump/$i | grep "VM Name:" | cut -d':' -f5 | cut -d' ' -f2)
+     echo "VM Name: $vmname"
+
      bkpname=$(echo $i | cut -d'.' -f1)
+     echo "Backup name: $bkpname"
+
      if [ -f "$directory/dump/$bkpname.vma.zst" ]
      then
           mv $directory/dump/$bkpname.vma.zst $directory/dump/$vmname-$bkpname.vma.zst
